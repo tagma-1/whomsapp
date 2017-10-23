8e6e9ae49de58276d71ca5c28cf3af873fd936b5
tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-10-23 06:54:13 +0000

    Add header

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,10 @@
 module MessagesHelper
+  
+  def emoji_display(content)
+    content.gsub(':smile:', '&#9786;')
+    content.gsub(':laughing:', '&#9786;')
+    content.gsub(':stuck_out_tongue:', '&#9786;')
+    content.gsub(':worried:', '&#9786;')
+    content.gsub(':wink:', '&#9786;')
+  end
 end
